2978c91a7dbb661beedf6e785aefe6719f2b351d
aleatory <josivalribeiro514@gmail.com>
2021-08-16 08:07:39 -0300

    First commit

---
+++ dono/dono.js
@@ -0,0 +1,24 @@
+const dono = (prefix) => {
+	return `
+━━━❮ 𝐌𝐄𝐍𝐔 𝐃𝐄 𝐃𝐎𝐍𝐎 ❯━━━
+◆◇  
+◆◇ ${prefix}serpremium
+◆◇ ${prefix}delpremium @(marca)
+◆◇ ${prefix}addpremium @(marca)
+◆◇ ${prefix}addlevel @(marca) (numero)
+◆◇ ${prefix}clonar [@] (rouba ft de prf)
+◆◇ ${prefix}setppbot (img, = foto do BT)
+◆◇ ${prefix}setnomebot (nome do bot qqr)
+◆◇ ${prefix}limpar (limpa tds conversas)
+◆◇ ${prefix}block [@] (bloq de usar cmds) 
+◆◇ ${prefix}unblock [@] (desbloquear) 
+◆◇ ${prefix}setprefix 
+◆◇ ${prefix}bc [texto] (ele faz uma™)
+◆◇  
+━━━❮ ${NamaBot} ❯━━━`
+
+}
+exports.dono = dono
+
+
+
